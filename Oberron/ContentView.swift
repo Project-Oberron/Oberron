3d193d2ac29f35f3c5866b729c93711336674e6e
Muhammad Akbar Reishandy <akbar@reishandy.id>
2026-08-08 06:58:07 +0800

    Initial Commit

---
+++ Oberron/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Oberron
+//
+//  Created by Muhammad Akbar Reishandy on 08/08/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Oberron/OberronApp.swift
@@ -0,0 +1,17 @@
+//
+//  OberronApp.swift
+//  Oberron
+//
+//  Created by Muhammad Akbar Reishandy on 08/08/26.
+//
+
+import SwiftUI
+
+@main
+struct OberronApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
